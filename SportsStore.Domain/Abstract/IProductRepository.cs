d63f56b5e2f818448b46320fac14e3d9b3e0c938
Sven Olding <sven.olding@gmx.de>
2018-12-31 18:52:54 +0100

    add abstract repository for products

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
