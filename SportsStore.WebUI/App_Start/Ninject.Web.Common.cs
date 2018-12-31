796e22540f1274f0c32fbab29ee335a30c765533
Sven Olding <sven.olding@gmx.de>
2018-12-31 10:33:57 +0100

    setup DI container

--- SportsStore.WebUI/App_Start/Ninject.Web.Common.cs
+++ SportsStore.WebUI/App_Start/Ninject.Web.Common.cs
@@ -5,11 +5,13 @@ namespace SportsStore.WebUI.App_Start
 {
     using System;
     using System.Web;
-
+    using System.Web.Mvc;
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
     using Ninject;
     using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
+    using SportsStore.WebUI.Infrastructure;
 
     public static class NinjectWebCommon 
     {
@@ -60,6 +62,7 @@ namespace SportsStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
         }        
     }
 }
