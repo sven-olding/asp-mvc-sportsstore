7d86deab5fec0210cca44f1d375046e37693f97c
Sven Olding <sven.olding@gmx.de>
2019-01-01 08:13:23 +0100

    add database context

---
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,13 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
