6cdefee011f86cbc2321743d92c4f7d54dc56528
Sven Olding <sven.olding@gmx.de>
2019-01-30 14:07:06 +0100

    add shopping cart domain entities

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private readonly List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection.Where(p => p.Product.ProductID == product.ProductID).FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine { Product = product });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(Product product)
+        {
+            lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(l => l.Quantity * l.Product.Price);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+}
---
+++ SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    /// <summary>
+    /// Summary description for CartTests
+    /// </summary>
+    [TestClass]
+    public class CartTests
+    {
+
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            // arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Cart cart = new Cart();
+            // act
+            cart.AddItem(p1, 2);
+            cart.AddItem(p2, 1);
+            CartLine[] results = cart.Lines.ToArray();
+            // assert
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Product, p1);
+            Assert.AreEqual(results[1].Product, p2);
+            Assert.AreEqual(results[0].Quantity, 2);
+        }
+    }
+}
