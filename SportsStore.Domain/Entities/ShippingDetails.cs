3b1be6a44d647a3610145fff05b151a5d118b9b6
Sven Olding <sven.olding@gmx.de>
2019-03-08 15:57:11 +0100

    reduced duplication in checkout form

---
+++ SportsStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Domain.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter the first address line")]
+        public string Line1 { get; set; }
+
+        public string Line2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter a city name")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter a zip code")]
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Please enter a country")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
