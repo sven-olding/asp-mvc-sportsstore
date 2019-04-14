3e06382770214a4ee2781afb6504131d03192bba
Sven Olding <sven.olding@gmx.de>
2019-04-14 18:47:18 +0200

    complete order processing with validation and emailing submitted orders

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -9,10 +9,12 @@ namespace SportsStore.WebUI.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private IOrderProcessor orderProcessor;
 
-        public CartController(IProductRepository repo)
+        public CartController(IProductRepository repo, IOrderProcessor processor)
         {
             repository = repo;
+            orderProcessor = processor;
         }
 
         public ViewResult Index(Cart cart, string returnUrl)
@@ -42,16 +44,34 @@ namespace SportsStore.WebUI.Controllers
                 cart.RemoveItem(product);
             }
             return RedirectToAction("Index", new { returnUrl });
-        }    
+        }
 
         public PartialViewResult Summary(Cart cart)
         {
             return PartialView(cart);
         }
-
         public ViewResult Checkout()
         {
             return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty");
+            }
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
         }
     }
 }
