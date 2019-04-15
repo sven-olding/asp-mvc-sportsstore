c0125fde5abb272390de582aa28c50a163ecc760
Sven Olding <sven.olding@gmx.de>
2019-04-15 15:17:29 +0200

    add admin controller and new unit test

---
+++ SportsStore.UnitTests/AdminTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            // arrange 
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3"}
+            });
+            AdminController controller = new AdminController(mock.Object);
+            // act
+            Product[] result = ((IEnumerable<Product>)controller.Index().ViewData.Model).ToArray();
+            // assert
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual(result[0].Name, "P1");
+            Assert.AreEqual(result[1].Name, "P2");
+            Assert.AreEqual(result[2].Name, "P3");            
+        }
+    }
+}
